db6445bc56ea053d21d73dfcbecca0e5a0677b07
Nathan Anderson <raynorue@gmail.com>
2020-06-07 23:27:33 -0500

    Prepublish for live... still need to setup jenkins..

--- prepublish-live.js
+++ prepublish-live.js
@@ -2,10 +2,10 @@ const fs = require('fs').promises;
 const path = require('path');
 const sv = require('semver');
 
-const packName = 'snich-canary';
-const packDisplayName = 'S.N.I.C.H. Canary';
-const description = 'ServiceNow Integrated Code Helper - Canary. Get the latest as I build stuff ready to be tested! Provide feedback to integratenate@gmail.com';
-const icon = 'images/icon-canary.png';
+const packName = 'snich';
+const packDisplayName = 'S.N.I.C.H.';
+const description = 'ServiceNow Integrated Code Helper. Do not worry, we will not tell anyone!';
+const icon = 'images/icon-shrunk.png';
 
 
 let updatePackageJSON = async function(){
@@ -13,9 +13,9 @@ let updatePackageJSON = async function(){
     var packageJSON = await fs.readFile(packagePath);
     let package = JSON.parse(packageJSON.toString());
 
-    let newVer = sv.inc(package.canaryVersion, 'prerelease', {includePrerelease:true,});
+    let newVer = sv.inc(package.liveVersion, 'minor');
     package.version = newVer;
-    package.canaryVersion = newVer;
+    package.liveVersion = newVer;
 
     package.name = packName;
     package.displayName = packDisplayName;
